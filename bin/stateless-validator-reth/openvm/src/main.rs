9d2ef377e84a58b3e45f1051b48b0e9b7c42d5e3
Gustavo Figueiredo <me@figtracer.com>
2026-08-11 11:21:13 +0100

    fix(openvm): initialize guest runtime

--- bin/stateless-validator-reth/openvm/src/main.rs
+++ bin/stateless-validator-reth/openvm/src/main.rs
@@ -3,6 +3,8 @@
 use ere_platform_openvm::OpenVMPlatform;
 use stateless_validator_reth::guest::{crypto::zkvm_interface, entrypoint};
 
+ere_platform_openvm::openvm::init!();
+
 fn main() {
     zkvm_interface::install_crypto();
     entrypoint::<OpenVMPlatform>();
